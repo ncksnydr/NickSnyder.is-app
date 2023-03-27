644a543b654e53ca348eef2c4fe11a44588462a6
Nick Snyder <nick@nicksnyder.is>
2023-03-27 14:07:04 -0700

    Add php-cs-fixer configuration file
    Note: php-cs-fixer is not currently working with the project. It may be an issue with the local versus global binaries.
    Signed-off-by: Nick Snyder <nick@nicksnyder.is>

---
+++ .php-cs-fixer.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types = 1);
+
+$rulesJson = __DIR__."/.php-cs-fixer-rules.json";
+$rules = '{"@PSR12": true}';
+
+if (file_exists($rulesJson)) {
+	$rules = json_decode(file_get_contents($rulesJson), true);
+}
+
+$finder = PhpCsFixer\Finder::create()
+	->exclude('bootstrap/cache')
+	->exclude('node_modules')
+	->exclude('database')
+	->exclude('storage')
+	->exclude('vendor')
+	->notName('*.xml')
+	->notName('*.yml')
+	->notName('*.lock')
+	->notName('*.json')
+	->in(__DIR__);
+
+return (new PhpCsFixer\Config())
+	->setUsingCache(true)
+	->setRules($rules)
+	->setIndent("\t")
+	->setLineEnding("\n")
+	->setFinder($finder);
