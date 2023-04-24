97669489285b50e0d7244119659f990fadbb64b1
Nick Snyder <nick@nicksnyder.is>
2023-04-23 17:07:59 -0700

    Update hero
    Signed-off-by: Nick Snyder <nick@nicksnyder.is>

--- resources/views/home/partials/hero.blade.php
+++ resources/views/home/partials/hero.blade.php
@@ -1,5 +1,5 @@
-
-  <div class="relative isolate px-6 pt-2 lg:px-8">
+<section id="hero">
+  <div class="relative isolate px-6 pt-2 lg:pt-10 lg:pb-20">
     <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
       <svg class="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678">
         <path fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)" fill-opacity=".15" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
@@ -15,7 +15,7 @@
     <div class="mx-auto max-w-2xl py-12 sm:py-9 lg:pt-24 lg:pb-12">
       <div class="text-center">
         <h1 class="headline">{!! $hero->headline !!}</h1>
-        <p class="subheadline">{!! $hero->subheadline !!}</p>
+        <p class="mt-6 subheadline">{!! $hero->subheadline !!}</p>
       </div>
     </div>
 		
@@ -31,3 +31,7 @@
       </svg>
     </div>
   </div>
+</section>
+
+
+
