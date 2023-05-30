0ebcf4f2196922b5bcf8132331d7f3fa7bf427aa
Nick Snyder <nick@nicksnyder.is>
2023-05-30 15:56:48 -0700

    Add Google Analytics
    Signed-off-by: Nick Snyder <nick@nicksnyder.is>

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 	<head>
+	
+		@env('production')
+			@include('layouts.partials.analytics')
+		@endenv
+
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		@include('layouts.partials.seo')
